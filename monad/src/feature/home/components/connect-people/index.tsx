5204edf10118e7e2138280e0aa25f88b06241c1c
Veeno <sebastien.cloiseau@gmail.com>
2024-04-27 22:04:33 +0200

    feature: worldwide section

--- monad/src/feature/home/components/connect-people/index.tsx
+++ monad/src/feature/home/components/connect-people/index.tsx
@@ -1,8 +1,7 @@
 "use client";
-import { Button } from "@/components/button";
-import { MediumFont } from "@/components/font";
 import { motion, useScroll, useTransform } from "framer-motion";
 import dynamic from "next/dynamic";
+import Link from "next/link";
 import { useRef } from "react";
 import { getArcs } from "./constant";
 
@@ -14,17 +13,38 @@ const World = dynamic(
 );
 
 export function ConnectPeople() {
-  const firstTitleRef = useRef(null);
-  const secTitleRef = useRef(null);
-  const scrollFirst = useScroll({
+  const firstTitleRef = useRef<HTMLHeadingElement>(null);
+  const secTitleRef = useRef<HTMLHeadingElement>(null);
+  const descriptionRef = useRef<HTMLParagraphElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const { scrollYProgress: scrollFirst } = useScroll({
     target: firstTitleRef,
-    offset: ["0 1.5", "5 1"],
+    offset: ["2 1", "3 1"],
   });
-  const scrollSec = useScroll({
+  const { scrollYProgress: scrollSec } = useScroll({
     target: secTitleRef,
-    offset: ["0 1", "5 1"],
+    offset: ["2.5 1", "3.5 1"],
   });
 
+  const { scrollYProgress: scrollDescription } = useScroll({
+    target: descriptionRef,
+    offset: ["1.33 1", "2 1"],
+  });
+  const { scrollYProgress: scrollBtn } = useScroll({
+    target: buttonRef,
+    offset: ["1.5 1", "2.3 1"],
+  });
+
+  const opacityProgress = useTransform(scrollFirst, [0, 1], [0, 1]);
+  const slideProgress = useTransform(scrollFirst, [0, 1], ["-50%", "0%"]);
+  const slideProgressSec = useTransform(scrollFirst, [0, 1], ["-50%", "0%"]);
+  const opacityProgressSec = useTransform(scrollSec, [0, 1], [0, 1]);
+
+  const opacityProgressDescription = useTransform(
+    scrollDescription,
+    [0, 1],
+    [0, 1]
+  );
   const globeConfig = {
     pointSize: 10,
     globeColor: "#836EF9",
@@ -50,35 +70,14 @@ export function ConnectPeople() {
   const colors = ["#5FEDDF", "#80f2e5", "#bff7f2"];
   const sampleArcs = getArcs(colors);
 
-  const slideProgess = useTransform(
-    scrollFirst.scrollYProgress,
-    [0, 1],
-    ["-50%", "0%"]
-  );
-  const opacityProgress = useTransform(
-    scrollFirst.scrollYProgress,
-    [0, 1],
-    [0, 1]
-  );
-  const slideProgessSec = useTransform(
-    scrollSec.scrollYProgress,
-    [0, 1],
-    ["-40%", "0%"]
-  );
-  const opacityProgressSec = useTransform(
-    scrollSec.scrollYProgress,
-    [0, 1],
-    [0, 1]
-  );
-
   return (
     <div className="flex flex-row items-center justify-center py-20 h-screen relative w-full connect-container">
       <div className="max-w-7xl mx-auto w-full relative h-full px-4 flex items-center">
         <div className="w-2/4 slide-title">
           <motion.h2
             style={{
-              translateX: slideProgess,
               opacity: opacityProgress,
+              y: slideProgress,
             }}
             ref={firstTitleRef}
             className="font-gramatika text-[72px] uppercase font-bold text-black dark:text-white max-w-2/4 leading-[70px] slide-title"
@@ -87,24 +86,40 @@ export function ConnectPeople() {
           </motion.h2>
           <motion.h2
             style={{
-              translateX: slideProgessSec,
               opacity: opacityProgressSec,
+              y: slideProgress,
             }}
             ref={secTitleRef}
             className="font-gramatika text-[72px] uppercase font-bold text-black dark:text-white max-w-2/4 leading-[70px] slide-title"
           >
             worldwide
           </motion.h2>
-          <MediumFont className="max-w-[90%] text-80 mt-5">
+          <motion.p
+            style={{ opacity: scrollDescription, y: slideProgressSec }}
+            ref={descriptionRef}
+            className="max-w-[90%] text-80 mt-5 text-lg font-hoves-pro-bold"
+          >
             Monad unifies the community worldwide. Wherever you are on the map,
             you are an integral part of a movement that transcends borders and
             cultural differences. Join Monad in this adventure where each
             individual brings their own unique contribution to our community
             fabric. Together, we form a vibrant and diverse global network.
-          </MediumFont>
-          <Button url="https://discord.com/invite/monad" className="mt-5">
-            Join the Discord
-          </Button>
+          </motion.p>
+
+          <motion.button
+            ref={buttonRef}
+            style={{ opacity: scrollBtn }}
+            className="mt-8 text-white font-bold flex rounded-md h-11 animate-shimmer font-hoves-pro-bold items-center justify-center border border-base-border bg-[linear-gradient(110deg,#836EF9,45%,#b5a8fa,55%,#836EF9)] 
+            bg-[length:200%_100%]  border-[#b5a8fa] w-[180px] focus:outline-none focus:ring-2  focus:ring-offset-2 transition-all duration-500 hover:tracking-wide"
+          >
+            <Link
+              href="https://discord.com/invite/monad"
+              target="_blank"
+              rel="noopener norefferer"
+            >
+              Join the discord
+            </Link>
+          </motion.button>
         </div>
         <div className="w-2/4 h-[600px]">
           <World data={sampleArcs} globeConfig={globeConfig} />
