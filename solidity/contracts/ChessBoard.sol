84a76d10097c56f6c367c80ee2e26f93c034cb0c
Veeno <sebastien.cloiseau@gmail.com>
2024-05-04 01:30:51 +0200

    feature: legal move for pawn

--- solidity/contracts/ChessBoard.sol
+++ solidity/contracts/ChessBoard.sol
@@ -44,11 +44,13 @@ contract Chess {
 
     event ValueLogged(string message, int256 dx);
 
-    function checkLegalMove(uint _xFrom, uint _yFrom, uint _xTo, uint _yTo, string memory _piece)  public returns(bool) {
+    function checkLegalMove(uint _xFrom, uint _yFrom, uint _xTo, uint _yTo, string memory _piece, bool _isFirstMove)  public returns(bool) {
         int256 dx = int256(_xTo) - int256(_xFrom);
         int256 dy = int256(_yTo) - int256(_yFrom);
         emit ValueLogged("Value is DX: ", dx);
         emit ValueLogged("Value is DY: ", dy);
+        uint absDx = absolute(dx);
+        uint absDy = absolute(dy);
         if(parseString(_piece) == parseString("Knight") ) {
             if((dy == 2 && dx == 1) || 
             (dy == 1 && dx == 2) || 
@@ -62,15 +64,24 @@ contract Chess {
              } 
              return false;
         } else if(parseString(_piece) == parseString("Bishop")) {
-          if(absolute(dy) == absolute(dx)) {
-            return true;
-          }
+          if(absDy == absDx) return true;
           return false;
         } else if(parseString(_piece) == parseString("Rook")) {
-          if(dx == 0 || dy == 0) {
-            return true;
-          }
+          if(dx == 0 || dy == 0) return true;
           return false;
+        } else if(parseString(_piece) == parseString("King")) {
+          if(absDx == 1 && absDy == 1 || absDx == 1 && absDy == 0 || absDx == 0 && absDy == 1) 
+            return true;
+          return false;
+        } else if(parseString(_piece) == parseString("Pawn")) {
+          if (_isFirstMove) {
+            if (dy == 1 && dx == 0) return true; 
+            if (dy == 2 && dx == 0) return true; 
+            return false;
+          } else {
+            if (dy == 1 && dx == 0) return true; 
+            return false;
+          }
         }
         return false;
     }
